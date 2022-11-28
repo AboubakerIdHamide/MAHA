89c2ee312d1b348c43efaae32346a79ab85c3597
ABDELMOUMEN MUSTAFA <106890180+ABDELMOUMEN-MUSTAFA@users.noreply.github.com>
2022-11-28 22:34:11 +0100

    Update Video.php

--- App/models/Video.php
+++ App/models/Video.php
@@ -1,23 +1,4 @@
 <?php
-<<<<<<< HEAD
-	/**
-	 * class Video
-	 */
-
-	class Video
-	{
-		private $connect;
-
-		public function __construct($database)
-		{
-			$this->connect = $database;
-		}
-
-		public function insertVideo($dataVideo){
-			$request = $this->connect->prepare("INSERT INTO 
-				videos(id_formation, nom_video, url_video, duree_video, miniature_video, description_video) 
-				VALUES (:formation, :nom, :url, :duree, :miniature, :description)"
-=======
 
 /**
  * class Video
@@ -27,7 +8,6 @@ class Video
 {
 	private $connect;
 
-<<<<<<< HEAD
 	public function __construct($database)
 	{
 		$this->connect = $database;
@@ -48,75 +28,6 @@ class Video
 		$request->bindParam(':miniature', $dataVideo['img']);
 		$request->bindParam(':description', $dataVideo['desc']);
 		$response = $request->execute();
-=======
-		public function insertVideo($dataVideo){
-			$request = $this->connect->prepare("INSERT INTO 
-				videos(id_formation, nom_video, url_video, duree_video, description_video) 
-				VALUES (:formation, :nom, :url, :duree, :description)"
->>>>>>> dd0c7cebe874638ddfed7dbcae2a8836d9f45125
-			);
-
-			$request->bindParam(':formation', $dataVideo['Idformation']);
-			$request->bindParam(':nom', $dataVideo['nomVideo']);
-			$request->bindParam(':url', $dataVideo['url']);
-			$request->bindParam(':duree', $dataVideo['duree']);
-<<<<<<< HEAD
-			$request->bindParam(':miniature', $dataVideo['img']);
-			$request->bindParam(':description', $dataVideo['desc']);
-			$response = $request->execute();
-
-			return $response;
-		}
-
-		public function getVideo($idFormation, $idVideo){
-			$request = $this->connect->prepare("SELECT * FROM videos WHERE id_formation = :id_formation AND id_video = :id_video");
-
-			$request->bindParam(':id_formation', $idFormation);
-			$request->bindParam(':id_video', $idVideo);
-			$request->execute();
-
-			$video = $request->fetch();
-			return $video;
-		}
-		
-		public function updateVideo($dataVideo){
-			$request = $this->connect->prepare("
-								UPDATE videos
-								SET nom_video = :nom,	
-									url_video = :url,	
-									duree_video = :duree,	
-									miniature_video = :miniature, 	
-									description_video = :description
-								WHERE id_formation = :id_formation AND id_video = :id_video");
-
-			$request->bindParam(':nom', $dataVideo['nomVideo']);
-			$request->bindParam(':url', $dataVideo['url']);
-			$request->bindParam(':duree', $dataVideo['duree']);
-			$request->bindParam(':miniature', $dataVideo['img']);
-			$request->bindParam(':description', $dataVideo['description']);
-			$request->bindParam(':id_formation', $dataVideo['idFormation']);
-			$request->bindParam(':id_video', $dataVideo['idVideo']);
-
-			$response = $request->execute();
-
-			return $response;
-		}
-
-		public function deteleVideo($idFormation, $idVideo)
-		{
-			$request = $this->connect->prepare("
-								DELETE FROM videos
-								WHERE id_formation = :id_formation AND id_video = :id_video");
-			$request->bindParam(':id_formation', $idFormation);
-			$request->bindParam(':id_video', $idVideo);
-			$response = $request->execute();
-			return $response;
-		}
-	}
-=======
-			$request->bindParam(':description', $dataVideo['desc']);
-			$response = $request->execute();
->>>>>>> 4015d1c6449891054e8aa91d8efbcd5fb863e5df
 
 		return $response;
 	}
@@ -190,4 +101,3 @@ class Video
 		return $response;
 	}
 }
->>>>>>> dd0c7cebe874638ddfed7dbcae2a8836d9f45125
