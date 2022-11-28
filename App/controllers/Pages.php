5b47fd8f844e184a963cdd204924cf8ef45a6f88
ABDELMOUMEN MUSTAFA <106890180+ABDELMOUMEN-MUSTAFA@users.noreply.github.com>
2022-11-28 22:05:03 +0100

    Update Pages.php

--- App/controllers/Pages.php
+++ App/controllers/Pages.php
@@ -1,25 +1,19 @@
 <?php
 	
 class Pages extends Controller {
-<<<<<<< HEAD
-    public function __construct()
-    {
-        $this->fomateurModel = $this->model("Formateur");
-        $this->etudiantModel = $this->model("Etudiant");
-        $this->formationModel = $this->model("Formation");
-        $this->inscriptionModel = $this->model("Inscription");
-    }
+	public function __construct()
+	 {
+		$this->fomateurModel = $this->model("Formateur");
+		$this->etudiantModel = $this->model("Etudiant");
+		$this->formationModel = $this->model("Formation");
+		$this->inscriptionModel = $this->model("Inscription");
+	  }
 	public function index()
-    {
-        $info = $this->formationModel->getPupalaireCourses();
-        $data = [
-            'info' => $info
-        ];
-        $this->view("pages/index", $data);
-=======
-	public function index()
-    {
-        $this->view("pages/index");
->>>>>>> dd0c7cebe874638ddfed7dbcae2a8836d9f45125
-    }
+   	{
+		$info = $this->formationModel->getPupalaireCourses();
+		$data = [
+		    'info' => $info
+		];
+		$this->view("pages/index", $data);
+  	}
 }
