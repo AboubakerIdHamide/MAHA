27b3616b3fbb914844ba3eba133dc5a3b218b223
ABDELMOUMEN MUSTAFA <106890180+ABDELMOUMEN-MUSTAFA@users.noreply.github.com>
2022-11-28 21:27:00 +0100

    Update config.php

--- App/config/config.php
+++ App/config/config.php
@@ -5,14 +5,7 @@ define('DB_NAME', "MAHA");
 define('DB_USER', "root");
 define('DB_PASS', "");
 
-
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
-<<<<<<< HEAD
-define('URLROOT', "http://localhost/MAHA-BACK_END/MAHA");
-define('SITENAME', "MAHA");
-=======
 define('URLROOT', "http://localhost/maha");
 define('SITENAME', "MAHA");
-
->>>>>>> dd0c7cebe874638ddfed7dbcae2a8836d9f45125
