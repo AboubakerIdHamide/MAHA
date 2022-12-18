0f11a4ecd29a750bced59ba558edea89f7245a56
Abdelmoumen Mustafa <abdelmoumen.mustafa@gmail.com>
2022-12-18 20:55:34 +0100

    links added to index page but style is bad

--- App/controllers/Pages.php
+++ App/controllers/Pages.php
@@ -1,22 +1,26 @@
 <?php
-	
-class Pages extends Controller {
+
+class Pages extends Controller
+{
 	public function __construct()
-	 {
+	{
 		$this->fomateurModel = $this->model("Formateur");
 		$this->etudiantModel = $this->model("Etudiant");
 		$this->formationModel = $this->model("Formation");
 		$this->inscriptionModel = $this->model("Inscription");
-	  }
+	}
 	public function index()
-   	{
+	{
 		$info = $this->formationModel->getPupalaireCourses();
-		foreach($info as $row){
-            $row->numbAcht = $this->inscriptionModel->countApprenantsOfFormation($row->IdFormteur, $row->IdFormation)['total_apprenants'];
-        }
+
+		foreach ($info as $row) {
+			$row->numbAcht = $this->inscriptionModel->countApprenantsOfFormation($row->IdFormteur, $row->IdFormation)['total_apprenants'];
+			$row->imgFormation = $this->pcloudFile()->getLink($row->imgFormation);
+			$row->imgFormateur = $this->pcloudFile()->getLink($row->imgFormateur);
+		}
 		$data = [
-		    'info' => $info
+			'info' => $info
 		];
 		$this->view("pages/index", $data);
-  	}
+	}
 }
