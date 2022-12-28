d4c745960ad5232a75573ab681c2a3a0cda342b6
Abdelmoumen Mustafa <abdelmoumen.mustafa@gmail.com>
2022-12-28 13:15:27 +0100

    utf-8 fixed

--- App/libraries/Controller.php
+++ App/libraries/Controller.php
@@ -15,7 +15,7 @@ class Controller
     {
         // DataBase Connection
         try {
-            $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+            $db = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME.";charset=utf8mb4", DB_USER, DB_PASS);
             $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die('Erreur : ' . $e->getMessage());
