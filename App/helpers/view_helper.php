db142f9683674d41388f856e903b7d3fa0f45d1a
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-07-27 12:02:20 +0100

    Add view helper file

---
+++ App/helpers/view_helper.php
@@ -0,0 +1,15 @@
+<?php
+
+// Load view helper
+function view($view, $data = [])
+{
+    // check for view file
+    if (file_exists('../App/views/' . $view . '.php')) {
+        extract($data);
+
+        // require that view
+        require_once "../App/views/" . $view . ".php";
+    } else {
+        require_once "../App/views/page404.php";
+    }
+}
