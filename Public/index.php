8680af83e56119e776b93e9530cfc2e168a1e15d
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-07-30 23:14:38 +0100

    Intansiate api controllers and call index method

--- Public/index.php
+++ Public/index.php
@@ -53,6 +53,8 @@ class Router
             require '../app/Controllers/api/' . $this->currentController . '.php';
             // initialize a contoller object
             $this->currentController = new $this->currentController;
+
+            call_user_func_array([$this->currentController, $this->currentMethod], $this->currentParams);
         } else {
             $this->isControllerExist($url[0]);
             require '../app/Controllers/' . $this->currentController . '.php';
