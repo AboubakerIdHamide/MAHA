a1a421a944a8dd3870f068aef1a90a1c2e58f217
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-03 13:39:07 +0100

    Remove unused code

--- Public/js/cours-details.js
+++ Public/js/cours-details.js
@@ -19,11 +19,6 @@
     }
   });
 
-  // Sticky sidebar
-  $("#sidebar").theiaStickySidebar({
-    additionalMarginTop: 150,
-  });
-
   // Mobile Mmenu
   var $menu = $("nav#menu").mmenu(
     {
@@ -37,7 +32,7 @@
         title: "MENU",
       },
       navbars: [
-        { position: "bottom", content: ['<a href="#0">© 2021 Udema</a>'] },
+        { position: "bottom", content: ['<a href="#0">© 2023 MAHA</a>'] },
       ],
     },
     {
@@ -67,257 +62,14 @@
     }, 100);
   });
 
-  // Header button explore
-  $('a[href^="#"].btn_explore').on("click", function (e) {
-    e.preventDefault();
-    var target = this.hash;
-    var $target = $(target);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $target.offset().top,
-        },
-        100,
-        "linear",
-        function () {
-          window.location.hash = target;
-        }
-      );
+  /* Search overlay */
+  $(".search-overlay-menu-btn").on("click", function (a) {
+    $(".search-overlay-menu").addClass("open"), 
+    $('.search-overlay-menu > form > input[type="search"]').focus()}), 
+    $(".search-overlay-close").on("click", function (a) {
+    $(".search-overlay-menu").removeClass("open")}),
+    $(".search-overlay-menu, .search-overlay-menu .search-overlay-close").on("click keyup", function (a) {
+    (a.target == this || "search-overlay-close" == a.target.className || 27 == a.keyCode) && $(this).removeClass("open")
   });
 
-  // WoW - animation on scroll
-  var wow = new WOW({
-    boxClass: "wow", // animated element css class (default is wow)
-    animateClass: "animated", // animation css class (default is animated)
-    offset: 0, // distance to the element when triggering the animation (default is 0)
-    mobile: true, // trigger animations on mobile devices (default is true)
-    live: true, // act on asynchronously loaded content (default is true)
-    callback: function (box) {
-      // the callback is fired every time an animation is started
-      // the argument that is passed in is the DOM node being animated
-    },
-    scrollContainer: null, // optional scroll container selector, otherwise use window
-  });
-  wow.init();
-
-  /*  video popups */
-  $(".video").magnificPopup({ type: "iframe" }); /* video modal*/
-
-  /*  Image popups */
-  $(".magnific-gallery").each(function () {
-    $(this).magnificPopup({
-      delegate: "a",
-      type: "image",
-      gallery: {
-        enabled: true,
-      },
-      removalDelay: 500, //delay removal by X to allow out-animation
-      callbacks: {
-        beforeOpen: function () {
-          // just a hack that adds mfp-anim class to markup
-          this.st.image.markup = this.st.image.markup.replace(
-            "mfp-figure",
-            "mfp-figure mfp-with-anim"
-          );
-          this.st.mainClass = this.st.el.attr("data-effect");
-        },
-      },
-      closeOnContentClick: true,
-      midClick: true, // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
-    });
-  });
-
-  // Accordion
-  function toggleChevron(e) {
-    $(e.target)
-      .prev(".card-header")
-      .find("i.indicator")
-      .toggleClass("ti-minus ti-plus");
-  }
-  $("#accordion_lessons").on(
-    "hidden.bs.collapse shown.bs.collapse",
-    toggleChevron
-  );
-  function toggleIcon(e) {
-    $(e.target)
-      .prev(".panel-heading")
-      .find(".indicator")
-      .toggleClass("ti-minus ti-plus");
-  }
-  // Accordion 2 (updated v1.2)
-  $(".accordion_2").on("hidden.bs.collapse shown.bs.collapse", toggleChevron);
-  function toggleIcon(e) {
-    $(e.target)
-      .prev(".panel-heading")
-      .find(".indicator")
-      .toggleClass("ti-minus ti-plus");
-  }
-  $(".panel-group").on("hidden.bs.collapse", toggleIcon);
-  $(".panel-group").on("shown.bs.collapse", toggleIcon);
-
-  // Input field effect
-  (function () {
-    if (!String.prototype.trim) {
-      (function () {
-        // Make sure we trim BOM and NBSP
-        var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-        String.prototype.trim = function () {
-          return this.replace(rtrim, "");
-        };
-      })();
-    }
-    [].slice
-      .call(
-        document.querySelectorAll("input.input_field, textarea.input_field")
-      )
-      .forEach(function (inputEl) {
-        // in case the input is already filled..
-        if (inputEl.value.trim() !== "") {
-          classie.add(inputEl.parentNode, "input--filled");
-        }
-
-        // events:
-        inputEl.addEventListener("focus", onInputFocus);
-        inputEl.addEventListener("blur", onInputBlur);
-      });
-    function onInputFocus(ev) {
-      classie.add(ev.target.parentNode, "input--filled");
-    }
-    function onInputBlur(ev) {
-      if (ev.target.value.trim() === "") {
-        classie.remove(ev.target.parentNode, "input--filled");
-      }
-    }
-  })();
-
-  // Selectbox
-  $(".selectbox").selectbox();
-
-  // Check and radio input styles
-  $("input.icheck").iCheck({
-    checkboxClass: "icheckbox_square-grey",
-    radioClass: "iradio_square-grey",
-  });
-
-  // Carousels
-  $("#carousel").owlCarousel({
-    center: true,
-    items: 2,
-    loop: true,
-    margin: 10,
-    responsive: {
-      0: {
-        items: 1,
-        dots: false,
-      },
-      600: {
-        items: 2,
-      },
-      1000: {
-        items: 4,
-      },
-    },
-  });
-
-  $("#reccomended").owlCarousel({
-    center: true,
-    items: 2,
-    loop: true,
-    margin: 0,
-    responsive: {
-      0: {
-        items: 1,
-      },
-      767: {
-        items: 2,
-      },
-      1000: {
-        items: 3,
-      },
-      1400: {
-        items: 4,
-      },
-    },
-  });
-
-  // Sticky filters
-  $(window).bind("load resize", function () {
-    var width = $(window).width();
-    if (width <= 991) {
-      $(".sticky_horizontal").stick_in_parent({
-        offset_top: 50,
-      });
-    } else {
-      $(".sticky_horizontal").stick_in_parent({
-        offset_top: 73,
-      });
-    }
-  });
-
-  // Secondary nav scroll
-  var $sticky_nav = $(".secondary_nav");
-  $sticky_nav.find("a").on("click", function (e) {
-    e.preventDefault();
-    var target = this.hash;
-    var $target = $(target);
-    $("html, body").animate(
-      {
-        scrollTop: $target.offset().top - 150,
-      },
-      200,
-      "linear"
-    );
-  });
-  $sticky_nav.find("ul li a").on("click", function () {
-    $sticky_nav.find("ul li a.active").removeClass("active");
-    $(this).addClass("active");
-  });
-
-  // Faq section (updated v1.2)
-  $('#faq_box a[href^="#"]').on("click", function () {
-    if (
-      location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") ||
-      location.hostname == this.hostname
-    ) {
-      var target = $(this.hash);
-      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-      if (target.length) {
-        $("html,body").animate(
-          {
-            scrollTop: target.offset().top - 185,
-          },
-          200
-        );
-        return false;
-      }
-    }
-  });
-  $("ul#cat_nav li a").on("click", function () {
-    $("ul#cat_nav li a.active").removeClass("active");
-    $(this).addClass("active");
-  });
-
-  // Search Bar
-  const searchForm = document.getElementById("searchform");
-  const searchIcon = document.getElementById("searchIcon");
-  const inputSearch = searchForm.querySelector("#input-search");
-
-  searchIcon.addEventListener("click", () => {
-    searchForm.classList.toggle("hide");
-    if (searchIcon.classList.contains("fa-search")) {
-      searchIcon.classList.replace("fa-search", "fa-xmark");
-    } else {
-      searchIcon.classList.replace("fa-xmark", "fa-search");
-    }
-  });
-
-  searchForm.onsubmit = (e) => {
-    let valRech = inputSearch.value;
-    if (!(valRech.length < 1 && valRech.length > 200)) {
-      window.location.href = `http://localhost/MAHA/pageFormation/rechercheFormations/${valRech}`;
-    }
-    e.preventDefault();
-  };
 })(window.jQuery);
