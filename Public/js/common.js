d126339429d013aa36b7851ed6d334cfebd76df4
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-04 21:26:43 +0100

    Remove unused code

--- Public/js/common.js
+++ Public/js/common.js
@@ -81,54 +81,6 @@
 				window.location.hash = target;
 			});
 		});
-	
-	// // WoW - animation on scroll
-	// var wow = new WOW(
-	//   {
-	// 	boxClass:     'wow',      // animated element css class (default is wow)
-	// 	animateClass: 'animated', // animation css class (default is animated)
-	// 	offset:       0,          // distance to the element when triggering the animation (default is 0)
-	// 	mobile:       true,       // trigger animations on mobile devices (default is true)
-	// 	live:         true,       // act on asynchronously loaded content (default is true)
-	// 	callback:     function(box) {
-	// 	  // the callback is fired every time an animation is started
-	// 	  // the argument that is passed in is the DOM node being animated
-	// 	},
-	// 	scrollContainer: null // optional scroll container selector, otherwise use window
-	//   }
-	// );
-	// wow.init();
-	  
-	// // Input field effect
-	// (function () {
-	// 	if (!String.prototype.trim) {
-	// 		(function () {
-	// 			// Make sure we trim BOM and NBSP
-	// 			var rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
-	// 			String.prototype.trim = function () {
-	// 				return this.replace(rtrim, '');
-	// 			};
-	// 		})();
-	// 	}
-	// 	[].slice.call(document.querySelectorAll('input.input_field, textarea.input_field')).forEach(function (inputEl) {
-	// 		// in case the input is already filled..
-	// 		if (inputEl.value.trim() !== '') {
-	// 			classie.add(inputEl.parentNode, 'input--filled');
-	// 		}
-
-	// 		// events:
-	// 		inputEl.addEventListener('focus', onInputFocus);
-	// 		inputEl.addEventListener('blur', onInputBlur);
-	// 	});
-	// 	function onInputFocus(ev) {
-	// 		classie.add(ev.target.parentNode, 'input--filled');
-	// 	}
-	// 	function onInputBlur(ev) {
-	// 		if (ev.target.value.trim() === '') {
-	// 			classie.remove(ev.target.parentNode, 'input--filled');
-	// 		}
-	// 	}
-	// })();
 
 	// // Check and radio input styles
 	// $('input.icheck').iCheck({
@@ -156,26 +108,7 @@
 	// 	}
 	// });
 	
-	// $('#reccomended').owlCarousel({
-	// 	center: true,
-	// 	items: 2,
-	// 	loop: true,
-	// 	margin: 0,
-	// 	responsive: {
-	// 		0: {
-	// 			items: 1
-	// 		},
-	// 		767: {
-	// 			items: 2
-	// 		},
-	// 		1000: {
-	// 			items: 3
-	// 		},
-	// 		1400: {
-	// 			items: 4
-	// 		}
-	// 	}
-	// });
+	
 
 	// // Sticky filters
 	// $(window).bind('load resize', function () {
