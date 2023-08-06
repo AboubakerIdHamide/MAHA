e6163361374919c0cf9b0554a8d43192e9aa677f
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-06 21:21:23 +0100

    Add theme to global constants

--- App/config/globalConstants.php
+++ App/config/globalConstants.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Models\Stocked;
+
+// Get Theme
+$stocked = new Stocked;
+$theme = $stocked->getThemeData();
+
 // Global Constants
 define('URLROOT', "http://localhost/maha");
 define('CSSROOT', URLROOT . '/public/css');
@@ -7,3 +13,4 @@ define('JSROOT', URLROOT . '/public/js');
 define('IMAGEROOT', URLROOT . '/public/images');
 define('APPROOT', dirname(dirname(__FILE__)));
 define('SITENAME', "MAHA");
+define('LOGO', URLROOT . '/public/'.$theme->logo);
