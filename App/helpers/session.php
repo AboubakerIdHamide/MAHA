5b8cac69e5b1d8f420a7916e01bb12e783cf0fb3
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-11 23:48:03 +0100

    change session helper

--- App/helpers/session.php
+++ App/helpers/session.php
@@ -1,9 +1,32 @@
 <?php
 
-function session($key, $value = null)
-{
-	if(!is_null($value)){
-		return $_SESSION[$key] = $value;
-	}
-	return $_SESSION[$key] ?? false;
+class SessionManager {
+    private $key;
+
+    public function __construct($key) {
+        $this->key = $key;
+    }
+
+    public function set($value) {
+        $_SESSION[$this->key] = $value;
+    }
+
+    public function get() {
+        return $_SESSION[$this->key] ?? false;
+    }
+
+    public function remove() {
+        if (isset($_SESSION[$this->key])) {
+            unset($_SESSION[$this->key]);
+        }
+    }
+
+    public function flush() {
+        session_unset(); 
+        session_destroy(); 
+    }
+}
+
+function session($key = "") {
+    return new SessionManager($key);
 }
