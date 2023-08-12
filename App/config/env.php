ad7a7f61be8b313e9bf37c110b1d1afb3cfb0f51
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-12 23:54:19 +0100

    add SMTP configuration to env.php

--- App/config/env.php
+++ App/config/env.php
@@ -4,3 +4,13 @@ define('DB_HOST', "localhost");
 define('DB_NAME', "MAHA");
 define('DB_USER', "root");
 define('DB_PASS', "");
+
+// Mailling
+// MAIL_MAILER=smtp
+// MAIL_HOST=live.smtp.mailtrap.io
+// MAIL_PORT=2525
+// MAIL_USERNAME=//your username
+// MAIL_PASSWORD=// your password
+// MAIL_ENCRYPTION=tls
+// MAIL_FROM_ADDRESS=from@example.com
+// MAIL_FROM_NAME=//your app name
