ce5bb88a1e973fefdd92bd846782f4066d4f3a33
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-12 10:09:36 +0100

    edit email verification page

--- App/views/auth/emailVerification.php
+++ App/views/auth/emailVerification.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="<?= IMAGEROOT ?>/favicon.ico" />
     <title><?= SITENAME ?> | Vérification d'email</title>
@@ -11,21 +10,21 @@
     <!-- Font Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
 </head>
-
 <body>
     <div class="container">
         <form action="" method="post" enctype="multipart/form-data">
             <div class="main-form-heading">
                 <h1 class="logo">
-                    <a href="<?= URLROOT . "/page"; ?>">
-                        <img src="<?= IMAGEROOT?>/MAHA.png" alt="logo platform">
+                    <a href="<?= URLROOT ?>">
+                        <img src="<?= LOGO ?>" alt="logo maha">
                     </a>
                 </h1>
             </div>
-            <p>Tu es presque! Nous avons envoyé un code de vérification à <span class="user-email"><?= $data[0]["email"] ?></span> .</p>
+            <p>Vous êtes presque! Nous avons envoyé un code de vérification à <span class="user-email">
+                <?= session('user_data')->get()["email"] ?></span> .</p>
             <label for="code">Entrez le code ici pour vérifier votre identité</label>
-            <input type="text" id="code" placeholder="code" name="code" value="<?= $data[1]["code"] ?>">
-            <span class="error" id="error"><?= $data[1]["code_err"] ?></span>
+            <input type="text" id="code" placeholder="code" name="code" value="<?= $code ?>">
+            <span class="error" id="error"><?= $code_err ?></span>
             <button class="submit-btn" id="sbmtBtn">Vérifier</button>
             <button class="resend-link" name="resend" value="true" id="resend">Renvoyer</button>
         </form>
