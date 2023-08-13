36976b33baf520f8ef3277d177f91894dc53bc5e
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-13 23:35:27 +0100

    set DB and Mailing credentials in env file

--- App/config/env.php
+++ App/config/env.php
@@ -6,11 +6,9 @@ define('DB_USER', "root");
 define('DB_PASS', "");
 
 // Mailling
-// MAIL_MAILER=smtp
-// MAIL_HOST=live.smtp.mailtrap.io
-// MAIL_PORT=2525
-// MAIL_USERNAME=//your username
-// MAIL_PASSWORD=// your password
-// MAIL_ENCRYPTION=tls
-// MAIL_FROM_ADDRESS=from@example.com
-// MAIL_FROM_NAME=//your app name
+define('MAIL_HOST', 'smtp.gmail.com');
+define('MAIL_PORT', 465);
+define('MAIL_USERNAME', 'mahateamisgi@gmail.com');
+define('MAIL_PASSWORD', 'fcpfxrepelndwpqr');
+define('MAIL_FROM_ADDRESS', 'mahateamisgi@gmail.com');
+define('MAIL_FROM_NAME', 'MAHA');
