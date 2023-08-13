d8aee2b3ba13e8f9195719cc72eb118ac591fe8e
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-13 23:37:37 +0100

    Add verify email templare

---
+++ App/views/emails/verify-email.php
@@ -0,0 +1,241 @@
+<!DOCTYPE html>
+<html lang="en" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
+    <head>
+        <title></title>
+        <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport"/>
+        <!--[if mso]><xml><o:OfficeDocumentSettings><o:PixelsPerInch>96</o:PixelsPerInch><o:AllowPNG/></o:OfficeDocumentSettings></xml><![endif]-->
+        <style>
+            * {
+                box-sizing: border-box;
+            }
+
+            body {
+                margin: 0;
+                padding: 0;
+            }
+
+            a[x-apple-data-detectors] {
+                color: inherit !important;
+                text-decoration: inherit !important;
+            }
+
+            #MessageViewBody a {
+                color: inherit;
+                text-decoration: none;
+            }
+
+            p {
+                line-height: inherit
+            }
+
+            .desktop_hide, .desktop_hide table {
+                mso-hide: all;
+                display: none;
+                max-height: 0px;
+                overflow: hidden;
+            }
+
+            .image_block img+div {
+                display: none;
+            }
+
+            @media (max-width: 620px) {
+                .desktop_hide table.icons-inner, .social_block.desktop_hide .social-table {
+                    display: inline-block !important;
+                }
+
+                .icons-inner {
+                    text-align: center;
+                }
+
+                .icons-inner td {
+                    margin: 0 auto;
+                }
+
+                .mobile_hide {
+                    display: none;
+                }
+
+                .row-content {
+                    width: 100% !important;
+                }
+
+                .stack .column {
+                    width: 100%;
+                    display: block;
+                }
+
+                .mobile_hide {
+                    min-height: 0;
+                    max-height: 0;
+                    max-width: 0;
+                    overflow: hidden;
+                    font-size: 0px;
+                }
+
+                .desktop_hide, .desktop_hide table {
+                    display: table !important;
+                    max-height: none !important;
+                }
+            }
+        </style>
+    </head>
+    <body style="background-color: #d9dffa; margin: 0; padding: 0; -webkit-text-size-adjust: none; text-size-adjust: none;">
+        <table cellpadding="0" cellspacing="0" class="nl-container" role="presentation" style="mso-table-lspace: 0pt;mso-table-rspace: 0pt;background-color: #d9dffa;width: 600px;margin: 50px auto;" width="100%">
+            <tbody>
+                <tr>
+                    <td>
+                        <table align="center" cellpadding="0" cellspacing="0" class="row row-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <table align="center" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;margin-bottom: 20px" width="600">
+                                            <tbody>
+                                                <tr>
+                                                     <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-top: 20px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+                                                        <table cellpadding="0" cellspacing="0" class="image_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+                                                            <tr>
+                                                                <td class="pad" style="width:100%;">
+                                                                    <div align="center" class="alignment" style="line-height:10px">
+                                                                        <img alt="logo maha" src="cid:logo" style="display: block;height: auto; border: 0;max-width: 160px;width: 100%;" width="160"/>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <table align="center" cellpadding="0" cellspacing="0" class="row row-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; background-color: #fff;border-radius: 10px;" width="100%">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <table align="center" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 15px; padding-left: 50px; padding-right: 50px; padding-top: 15px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;" width="100%">
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-1" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#506bec;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:25px;line-height:120%;text-align:center;mso-line-height-alt:45.6px;">
+                                                                        <p style="margin: 0; word-break: break-word;">
+                                                                            <strong>
+                                                                                <span>Veuillez vérifier votre adresse e-mail</span>
+                                                                            </strong>
+                                                                        </p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-2" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#40507a;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:16px;line-height:120%;text-align:left;mso-line-height-alt:19.2px;">
+                                                                        <p style="margin: 0;">Merci de vous être inscrit chez nous. Cliquez sur le bouton ci-dessous pour vérifier votre adresse e-mail.</p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                        <table cellpadding="0" cellspacing="0" class="button_block block-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+                                                            <tr>
+                                                                <td class="pad" style="padding-bottom:20px;padding-left:10px;padding-right:10px;padding-top:20px;text-align:center;">
+                                                                    <div align="center" class="alignment">
+                                                                        <!--[if mso]><v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" href="http://www.example.com/" style="height:46px;width:308px;v-text-anchor:middle;" arcsize="35%" stroke="false" fillcolor="#506bec"><w:anchorlock/><v:textbox inset="5px,0px,0px,0px"><center style="color:#ffffff; font-family:Arial, sans-serif; font-size:15px"><![endif]-->
+                                                                        <a href="::tokenLink" style="text-decoration:none;display:inline-block;color:#ffffff;background-color:#506bec;border-radius:16px;width:auto;border-top:0px solid TRANSPARENT;font-weight:undefined;border-right:0px solid TRANSPARENT;border-bottom:0px solid TRANSPARENT;border-left:0px solid TRANSPARENT;padding-top:8px;padding-bottom:8px;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:15px;text-align:center;mso-border-alt:none;word-break:keep-all;" target="_blank">
+                                                                            <span style="padding-left:25px;padding-right:20px;font-size:15px;display:inline-block;
+                                                                            letter-spacing:normal;">
+                                                                                <span style="word-break:break-word;">
+                                                                                    <span data-mce-style="" style="line-height: 30px;text-transform: uppercase">
+                                                                                        <strong>Vérifier mon adresse e-mail</strong>
+                                                                                    </span>
+                                                                                </span>
+                                                                            </span>
+                                                                        </a>
+                                                                        <!--[if mso]></center></v:textbox></v:roundrect><![endif]-->
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-5" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#40507a;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:14px;line-height:120%;text-align:center;mso-line-height-alt:16.8px;">
+                                                                        <p style="margin: 0;">Si le bouton ci-dessus ne fonctionne pas pour vous, copiez et collez ce qui suit dans la barre d'adresse de votre navigateur:</p>
+                                                                        <p style="margin: 0;">
+                                                                            <a href="::tokenLink" rel="noopener" style="text-decoration: underline; color: #40507a;" target="_blank">::tokenLink</a>
+                                                                        </p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-6" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#6c757d;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:14px;line-height:120%;text-align:center;mso-line-height-alt:16.8px;">
+                                                                        <p style="margin: 0; word-break: break-word;">Si cet e-mail ne vous était pas destiné, n'hésitez pas à le supprimer.</p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table align="center" cellpadding="0" cellspacing="0" class="row row-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt;" width="100%">
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <table align="center" cellpadding="0" cellspacing="0" class="row-content stack" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; color: #000; width: 600px; margin: 0 auto;" width="600">
+                                            <tbody>
+                                                <tr>
+                                                    <td class="column column-1" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; font-weight: 400; text-align: left; padding-bottom: 20px; padding-left: 10px; padding-right: 10px; padding-top: 10px; vertical-align: top; border-top: 0px; border-right: 0px; border-bottom: 0px; border-left: 0px;background-color: #d9dffa" width="100%">
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-4" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#97a2da;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:14px;line-height:120%;text-align:center;mso-line-height-alt:16.8px;">
+                                                                        <p style="margin: 0; word-break: break-word;">
+                                                                            Ce lien expirera dans les prochaines ::expirationTime.<br/>N'hésitez pas à nous contacter à mahateamisgi@gmail.com.
+                                                                        </p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                        <table cellpadding="10" cellspacing="0" class="paragraph_block block-5" role="presentation" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; word-break: break-word;" width="100%">
+                                                            <tr>
+                                                                <td class="pad">
+                                                                    <div style="color:#97a2da;font-family:Helvetica Neue, Helvetica, Arial, sans-serif;font-size:12px;line-height:120%;text-align:center;mso-line-height-alt:14.399999999999999px;">
+                                                                        <p style="margin: 0; word-break: break-word;">
+                                                                            <span>Copyright© 2023 MAHA.</span>
+                                                                        </p>
+                                                                    </div>
+                                                                </td>
+                                                            </tr>
+                                                        </table>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- End -->
+    </body>
+</html>
