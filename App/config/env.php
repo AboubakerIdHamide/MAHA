da7ceb9c02f0cedc5e0d60f938dda2129838991e
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-14 23:25:38 +0100

    Google Auth Credentials

--- App/config/env.php
+++ App/config/env.php
@@ -12,3 +12,8 @@ define('MAIL_USERNAME', 'mahateamisgi@gmail.com');
 define('MAIL_PASSWORD', 'fcpfxrepelndwpqr');
 define('MAIL_FROM_ADDRESS', 'mahateamisgi@gmail.com');
 define('MAIL_FROM_NAME', 'MAHA');
+
+// Google Auth
+define('CLIENT_ID', '778408900492-5bsveu5t2lffq6fi85vp6eugf70frguf.apps.googleusercontent.com');
+define('CLIENT_SECRET', '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');
+define('REDIRECT_URI', 'http://localhost/maha/user/login');
