e8b7beeaac582cbed6eb730930dde5313d2c1559
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-15 23:29:07 +0100

    Refactore email confirm

--- App/views/auth/confirm.php
+++ App/views/auth/confirm.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="icon" type="image/x-icon" href="<?= IMAGEROOT ?>/favicon.ico" />
-    <title>Confirmation de mot de passe | <?= SITENAME ?></title>
+    <title>Email Confirmed | <?= SITENAME ?></title>
     <!-- GOOGLE WEB FONT -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
     <!-- Font Icons -->
@@ -22,29 +22,25 @@
     </div>
     <!-- End Preload -->
     <div class="container">
-        <form method="post" id="reset-form">
-            <h1 class="logo">
+        <div class="wrapper">
+            <h1 class="logo mb-3">
                 <a href="<?= URLROOT ?>">
                     <img src="<?= LOGO ?>" alt="logo maha">
                 </a>
             </h1>
             <div>
-                <h4>Réinitialiser le mot de passe</h4>
-                <p>Entrez votre adresse e-mail et nous vous enverrons un e-mail.</p>
+                <h4 class="my-3"><i class="fa-regular fa-circle-check" style="color: #25d45a;font-size: 5rem"></i></h4>
+                <div class="text-center">
+                    <p class="fw-bolder">Toutes nos félicitations!</p> 
+                    Votre adresse e-mail a été vérifiée avec succès.
+                </div>
             </div>
-            <div class="form-group">
-                <input type="password" id="password" name="password" class="form-control" placeholder="Nouveau mot de passe" />
-            </div>
-            <div class="form-group">
-                <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" placeholder="Confirmer nouveau mot de passe" />
-            </div>
-            <input type="hidden" name="token" value="<?= $_GET['token'] ?>"  />
-            <button class="btn_1 full-width" id="reset" type="submit">Confirmer</button>
-        </form>
+            <a href="<?= URLROOT ?>/<?= session('user')->get()->type ?>" class="btn_1 full-width">aller au DASHBOARD</a>
+        </div>
     </div>
     <script src="<?= JSROOT ?>/plugins/jquery-3.6.3.min.js"></script>
     <script src="<?= JSROOT ?>/plugins/jquery.validate.min.js"></script>
-    <script src="<?= JSROOT ?>/forgotpassword.js"></script>
+    <script src="<?= JSROOT ?>/auth/forgot.js"></script>
 </body>
 
 </html>
