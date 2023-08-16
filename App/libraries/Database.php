39e7a3278688b67763c8a8eb0a1ee67c5c84d657
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-17 00:05:33 +0100

    add env loader to Database library

--- App/libraries/Database.php
+++ App/libraries/Database.php
@@ -8,11 +8,13 @@ class Database
 
     public static function getConnection()
     {
-        require_once '../App/config/env.php';
-
         if (self::$connection === null) {
+            // Load Environment Variables (.env)
+            $dotenv = \Dotenv\Dotenv::createImmutable(dirname(dirname(__DIR__)));
+            $dotenv->load();
+
             try {
-                self::$connection = new \PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ";charset=utf8mb4", DB_USER, DB_PASS);
+                self::$connection = new \PDO('mysql:host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'] . ";charset=utf8mb4", $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD']);
                 self::$connection->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             } catch (\PDOException $e) {
                 // Handle connection errors
