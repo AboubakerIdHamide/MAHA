a7d0209b6b8c7e76952b01f9671b110689a656eb
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-16 23:16:34 +0100

    add Linkedin auth and twitter auth credentials

--- App/config/env.php
+++ App/config/env.php
@@ -13,7 +13,16 @@ define('MAIL_PASSWORD', 'fcpfxrepelndwpqr');
 define('MAIL_FROM_ADDRESS', 'mahateamisgi@gmail.com');
 define('MAIL_FROM_NAME', 'MAHA');
 
-// Google Auth
-define('CLIENT_ID', '778408900492-5bsveu5t2lffq6fi85vp6eugf70frguf.apps.googleusercontent.com');
-define('CLIENT_SECRET', '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');
 define('REDIRECT_URI', 'http://localhost/maha/user/login');
+
+// Google Auth
+define('CLIENT_ID_GOOGLE', '778408900492-5bsveu5t2lffq6fi85vp6eugf70frguf.apps.googleusercontent.com');
+define('CLIENT_SECRET_GOOGLE', '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');
+
+// LinkedIn Auth
+define('CLIENT_ID_LINKEDIN', '78xbkhozrwulj8');
+define('CLIENT_SECRET_LINKEDIN', 'N57FvZA6M8JZF3fv');
+
+// Twitter Auth
+define('CLIENT_ID_TWITTER', 'zoMQDEk0JuO9QfDaPFJDG4Bya');
+define('CLIENT_SECRET_TWITTER', 'f3GwDByHhWfan8PrvuPIg7IVFN2VISW2xiaS8ETL5KeBqYwsMQ');
