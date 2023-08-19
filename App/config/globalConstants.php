b933a5e9e9f04b69100c46b7e933882cd2682b2a
Mustafa ABDELMOUMEN <abdelmoumen.mustafa@gmail.com>
2023-08-19 23:21:21 +0100

    dd VIDEOROOT constant (absolute path to video public folder)

--- App/config/globalConstants.php
+++ App/config/globalConstants.php
@@ -11,6 +11,7 @@ define('URLROOT', "http://localhost/maha");
 define('CSSROOT', URLROOT . '/public/css');
 define('JSROOT', URLROOT . '/public/js');
 define('IMAGEROOT', URLROOT . '/public/images');
+define('VIDEOROOT', URLROOT . '/public/videos');
 define('APPROOT', dirname(dirname(__FILE__)));
 define('SITENAME', "MAHA");
 define('LOGO', URLROOT . '/public/'.$theme->logo);
