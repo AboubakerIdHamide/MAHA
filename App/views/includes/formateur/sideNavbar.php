0ca79537d82e053857eb1c84f8aca9bc7340b766
ABDELMOUMEN Mustafa <abdelmoumen.mustafa@gmail.com>
2023-08-31 01:49:35 +0100

    make current link active in sideBar, highlight the link if current page matches the url

--- App/views/includes/formateur/sideNavbar.php
+++ App/views/includes/formateur/sideNavbar.php
@@ -5,23 +5,29 @@
             <div class="sidebar-p-y">
                 <div class="sidebar-heading">Instructor</div>
                 <ul class="sidebar-menu sm-active-button-bg">
+                    <li class="sidebar-menu-item <?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/maha/formateur' ? 'active' : '' ?>">
+                        <a class="sidebar-menu-button"
+                           href="<?= URLROOT ?>/formateur">
+                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">dashboard</i> Dashboard
+                        </a>
+                    </li>
                     <li class="sidebar-menu-item">
                         <a class="sidebar-menu-button"
                            data-toggle="collapse"
                            href="#course_menu">
-                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">import_contacts</i>
+                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">desktop_mac</i>
                             Manage Courses
                             <span class="ml-auto sidebar-menu-toggle-icon"></span>
                         </a>
                         <ul class="sidebar-submenu sm-indent collapse"
                             id="course_menu">
-                            <li class="sidebar-menu-item">
+                            <li class="sidebar-menu-item <?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/maha/courses' ? 'active' : '' ?>">
                                 <a class="sidebar-menu-button"
                                    href="<?= URLROOT ?>/courses">
                                     <span class="sidebar-menu-text">Courses</span>
                                 </a>
                             </li>
-                            <li class="sidebar-menu-item">
+                            <li class="sidebar-menu-item <?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/maha/courses/add' ? 'active' : '' ?>">
                                 <a class="sidebar-menu-button"
                                    href="<?= URLROOT ?>/courses/add">
                                     <span class="sidebar-menu-text">Add Course</span>
@@ -29,22 +35,16 @@
                             </li>
                         </ul>
                     </li>
-                    <li class="sidebar-menu-item">
+                    <li class="sidebar-menu-item <?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/maha/formateur/earnings' ? 'active' : '' ?>">
                         <a class="sidebar-menu-button"
-                           href="instructor-quizzes.html">
-                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">help</i> Quiz Manager
-                        </a>
-                    </li>
-                    <li class="sidebar-menu-item">
-                        <a class="sidebar-menu-button"
-                           href="instructor-earnings.html">
+                           href="<?= URLROOT ?>/formateur/earnings">
                             <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">trending_up</i> Earnings
                         </a>
                     </li>
-                    <li class="sidebar-menu-item">
+                    <li class="sidebar-menu-item <?= parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) === '/maha/formateur/transactions' ? 'active' : '' ?>">
                         <a class="sidebar-menu-button"
-                           href="instructor-statement.html">
-                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">receipt</i> Statement
+                           href="<?= URLROOT ?>/formateur/transactions">
+                            <i class="sidebar-menu-icon sidebar-menu-icon--left material-icons">receipt</i> Transactions
                         </a>
                     </li>
                 </ul>
